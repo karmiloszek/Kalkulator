22eeb8761c403d649a83b7d753616434fa6c85d2
Lorua69 <kredka.pl@interia.pl>
2023-03-02 22:27:15 +0100

    Pobieranie liczb

--- Kalkulator2/Program.cs
+++ Kalkulator2/Program.cs
@@ -1,4 +1,12 @@
-﻿Console.WriteLine("KALKULATOR\n");
+﻿double PobierzLiczbe()
+{
+    if (!double.TryParse(Console.ReadLine(), out double input))
+        throw new Exception("Podana wartość nie jest liczbą!\n");
+
+    return input;
+}
+
+Console.WriteLine("KALKULATOR\n");
 for (; ; )
 {
     try
@@ -11,7 +19,24 @@ for (; ; )
             opcja = char.ToUpper(Convert.ToChar(Console.ReadLine()));
             if (opcja == 'A')
             {
+                Console.WriteLine("Podaj 1 liczbę:");
+                double num1 = PobierzLiczbe();
 
+                Console.WriteLine("Podaj operację.\nMożliwe operacje:\n'+' dodawanie,\n'-' odejmowanie,\n'*' mnożenie,\n'/' dzielenie,\n'^' potęgowanie,\n '%' procent liczby 1 z 2.");
+                char operacja;
+                try
+                {
+                    operacja = Convert.ToChar(Console.ReadLine());
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Niepoprawna operacja!");
+                    Console.Clear();
+                    continue;
+                }
+
+                Console.WriteLine("Podaj 2 liczbę:");
+                double num2 = PobierzLiczbe();
             }
             else if (opcja == 'B')
             {
