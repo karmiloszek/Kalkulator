15698c6dc02713f4cae24192c361b61107f09a7f
karmiloszek <karmiloszek@gmail.com>
2023-03-02 22:40:16 +0100

    Test jednostkowy

--- Testy/UnitTest1.cs
+++ Testy/UnitTest1.cs
@@ -1,11 +1,15 @@
+
+using Biblioteka;
+
 namespace Testy
 {
     public class UnitTest1
     {
+        RownanieKwadratowe kw = new RownanieKwadratowe(1, 2, -3);
         [Fact]
         public void Test1()
         {
-
+            Assert.Equal(new double[] { 1, -3 }, kw.Wynik());
         }
     }
 }
