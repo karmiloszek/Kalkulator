ab35a05c7fc5022800922133c863d5b53b958f92
karmiloszek <karmiloszek@gmail.com>
2023-03-02 22:29:29 +0100

    Obliczanie działań

--- Kalkulator2/Program.cs
+++ Kalkulator2/Program.cs
@@ -6,6 +6,27 @@
     return input;
 }
 
+double Oblicz(double num1, double num2, char oper)
+{
+    switch (oper)
+    {
+        case '+':
+            return num1 + num2;
+        case '-':
+            return num1 - num2;
+        case '*':
+            return num1 * num2;
+        case '/':
+            return num1 / num2;
+        case '^':
+            return Math.Pow(num1, num2);
+        case '%':
+            return num1 / 100 * num2;
+        default:
+            throw new Exception("Niepoprawna operacja!\n");
+    }
+}
+
 Console.WriteLine("KALKULATOR\n");
 for (; ; )
 {
@@ -37,6 +58,13 @@ for (; ; )
 
                 Console.WriteLine("Podaj 2 liczbę:");
                 double num2 = PobierzLiczbe();
+
+                double wynik = Oblicz(num1, num2, operacja);
+
+                Console.Clear();
+
+                Console.WriteLine("KALKULATOR\n");
+                Console.WriteLine(num1 + " " + operacja + " " + num2 + " = " + Math.Round(wynik, 2) + "\n");
             }
             else if (opcja == 'B')
             {
