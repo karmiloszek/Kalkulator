b3e07b974da542cde1de6d48a867090f38de7c1e
karmiloszek <karmiloszek@gmail.com>
2023-03-02 22:24:20 +0100

    Dwie opcje

--- Kalkulator2/Program.cs
+++ Kalkulator2/Program.cs
@@ -3,7 +3,27 @@ for (; ; )
 {
     try
     {
+        Console.WriteLine("Wybierz opcję:\nA - kalkulator\nB-równanie kwadratowe");
 
+        char opcja;
+        try
+        {
+            opcja = char.ToUpper(Convert.ToChar(Console.ReadLine()));
+            if (opcja == 'A')
+            {
+
+            }
+            else if (opcja == 'B')
+            {
+
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Niepoprawna opcja!");
+            continue;
+        }
+        
     }
     catch (Exception ex)
     {
